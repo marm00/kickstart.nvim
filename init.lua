216ff87f72b022c9d7c4489feebff871e140f0bc
marm00 <markscheperman@gmail.com>
2025-01-03 21:53:47 +0100

    ruby lsp & treesitter

--- init.lua
+++ init.lua
@@ -647,7 +647,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        ruby_lsp = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -918,7 +918,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
